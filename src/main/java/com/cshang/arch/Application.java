22341c86e5ce164ed349e53f104d26b92d618bb0
chris.shang <chris.shang@sinobot.com.cn>
2018-12-04 15:22:55 +0800

    add spring boot support

---
+++ src/main/java/com/cshang/arch/Application.java
@@ -0,0 +1,14 @@
+package com.cshang.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.cshang.arch")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/cshang/arch/config/CoreConfiguration.java
@@ -0,0 +1,4 @@
+package com.cshang.arch.config;
+
+public class CoreConfiguration {
+}
